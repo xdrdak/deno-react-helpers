4843bbe36288ea0be7ed5840177435f53a1730f5
Xavier Drdak <1198051+xdrdak@users.noreply.github.com>
2020-05-15 21:35:18 -0400

    Use denopkg as source for demo app

--- demo/deps.ts
+++ demo/deps.ts
@@ -1,15 +1,8 @@
-import styledPkg from "https://dev.jspm.io/styled-components";
-
-// @ts-ignore
-const styled = styledPkg.default;
-// @ts-ignore
-const ServerStyleSheet = styledPkg.ServerStyleSheet;
-
-export { styled, ServerStyleSheet };
-
 import * as React from "https://cdn.pika.dev/react@^16.13.1";
 export { React };
 
-// @ts-ignore
-import ReactDOMServer from "https://dev.jspm.io/react-dom/server";
-export { ReactDOMServer };
+import { ReactDOMServer } from "https://denopkg.com/xdrdak/deno-react-helpers/modules/ReactDomServer/mod.ts";
+export { ReactDOMServer }
+
+import { renderToStaticMarkup } from "https://denopkg.com/xdrdak/deno-react-helpers/modules/debass/mod.ts";
+export { renderToStaticMarkup }
--- demo/mod.ts
+++ demo/mod.ts
@@ -1,8 +1,5 @@
-import * as React from "https://cdn.pika.dev/react@^16.13.1";
-
+import { React, ReactDOMServer, renderToStaticMarkup } from './deps.ts';
 import { App } from "./App.tsx";
-import { ReactDOMServer } from "../modules/ReactDomServer/mod.ts";
-import { renderToStaticMarkup } from "../modules/debass/mod.ts";
 
 const { html, styleTags } = renderToStaticMarkup(
   ReactDOMServer.renderToStaticMarkup,
